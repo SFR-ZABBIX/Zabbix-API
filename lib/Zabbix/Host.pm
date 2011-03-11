671a17d2962b8731c946d38fe8d3cbb1f765557a
Fabrice Gabolde <fabrice.gabolde@uperto.com>
2011-03-11 18:12:43 +0100

    Updated documentation for Zabbix::Host: documented new()
    (documentation was stub)

--- lib/Zabbix/Host.pm
+++ lib/Zabbix/Host.pm
@@ -54,41 +54,49 @@ Zabbix::Host -- Base class for Zabbix hosts
 
 =head1 SYNOPSIS
 
-  use Zabbix::Host;
+  use Zabbix;
 
-  # TODO write the rest
+  my $zabbix = Zabbix->new(...);
+
+  my $host = $zabbix->get_hosts(hostnames => ['Zabbix Server'])->[0];
 
 =head1 DESCRIPTION
 
-TODO write this
+This class doesn't do much beyond blessing a hashref of data (provided by the
+Zabbix API).
 
 =head1 ATTRIBUTES
 
-=over 4
-
-=item attribute1
-
-TODO write this
-
-=item attribute2
-
-TODO write this
-
-=back
+None.
 
 =head1 METHODS
 
 =over 4
 
-=item method(args)
+=item new(%params)
 
-TODO write this
+This is the main constructor for the Zabbix::Host class.  The following
+parameters are required at present: _root port ip status hostid error macros
+host.
+
+All these can be fetched through the API with the C<host.get> JSON-RPC method,
+although the Zabbix C<get_hosts> method or the Zabbix::Item C<get_host> method
+are more convenient (they also return Zabbix::Host instances rather than raw
+hashrefs).
+
+The constructor filters the parameters hash so that only the required keys are
+present before the blessed ref is returned.  An exception will be thrown if
+one or more required keys are missing:
+
+  "$class->new is missing parameters: ..."
 
 =back
 
 =head1 SEE ALSO
 
-TODO links to other pods and documentation
+L<Zabbix>
+
+The Zabbix API documentation, at L<http://www.zabbix.com/documentation/start>
 
 =head1 AUTHOR
 
