5c0554a33a7f648a18fc90d18fce3d109325d7d1
Fabrice Gabolde <fabrice.gabolde@uperto.com>
2011-08-03 10:48:26 +0200

    Basic Map support.

---
+++ lib/Zabbix/API/Map.pm
@@ -0,0 +1,120 @@
+package Zabbix::API::Map;
+
+use strict;
+use warnings;
+use 5.010;
+use Carp;
+
+use parent qw/Zabbix::API::CRUDE/;
+
+sub id {
+
+    ## mutator for id
+
+    my ($self, $value) = @_;
+
+    if (defined $value) {
+
+        $self->data->{sysmapid} = $value;
+        return $self->data->{sysmapid};
+
+    } else {
+
+        return $self->data->{sysmapid};
+
+    }
+
+}
+
+sub prefix {
+
+    my (undef, $suffix) = @_;
+
+    if ($suffix) {
+
+        return 'map'.$suffix;
+
+    } else {
+
+        return 'map';
+
+    }
+
+}
+
+sub extension {
+
+    return ( output => 'extend',
+             select_selements => 'extend' );
+
+}
+
+sub collides {
+
+    my $self = shift;
+
+    return @{$self->{root}->query(method => $self->prefix('.get'),
+                                  params => { search => { name => $self->data->{name} }})};
+
+}
+
+1;
+__END__
+=pod
+
+=head1 NAME
+
+Zabbix::API::Item -- Zabbix item objects
+
+=head1 SYNOPSIS
+
+  use Zabbix::API::Item;
+
+  # TODO write the rest
+
+=head1 DESCRIPTION
+
+Handles CRUD for Zabbix graph objects.
+
+This is a subclass of C<Zabbix::API::CRUDE>.
+
+=head1 METHODS
+
+=over 4
+
+=item collides()
+
+Returns true if the item exists with this key on this hostid, false otherwise.
+
+=item host()
+
+Accessor for a local C<host> attribute, which it also happens to set from the
+server data if it isn't set already.
+
+=back
+
+=head1 EXPORTS
+
+Way too many constants, but for once they're documented (here:
+L<http://www.zabbix.com/documentation/1.8/api/item/constants>).
+
+Nothing is exported by default; you can use the tags C<:item_types>,
+C<:value_types>, C<:data_types> and C<:status_types> (or import by name).
+
+=head1 SEE ALSO
+
+L<Zabbix::API::CRUDE>.
+
+=head1 AUTHOR
+
+Fabrice Gabolde <fabrice.gabolde@uperto.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 SFR
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself, either Perl version 5.10.0 or, at your option,
+any later version of Perl 5 you may have available.
+
+=cut
